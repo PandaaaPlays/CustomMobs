38939724c457b9beae1366c507c7a5d8572289f3
etienne.toupin <etienne.toupin@usherbrooke.ca>
2025-06-14 22:36:38 -0400

    Test

--- CustomMobs.Plugin/src/main/java/ca/pandaaa/custommobs/custommobs/Options/Cow.java
+++ CustomMobs.Plugin/src/main/java/ca/pandaaa/custommobs/custommobs/Options/Cow.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class Cow extends CustomMobOption {
     /**
-     * TestdawdSpecifies the variant of the cow CustomMob, such as normal or a biome-specific type. When this value is
+     * Specifies the variant of the cow CustomMob, such as normal or a biome-specific type. When this value is
      * not set, the variant will be based on the biome it is spawned in (like it would by spawning a normal cow).
      */
     private static final String COW_VARIANT = "mob.cow-variant";
